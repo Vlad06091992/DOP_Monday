8f3c94edbda885681404d7839a482a93241cb3c7
Vlad <Smirnov.ru92@mail.ru>
2022-10-21 20:08:05 +0500

    edit files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {json} from "stream/consumers";
+
+
+type PropsType = {
+  id:number,
+  userId:number,
+  title:string
+  completed:boolean
+}
 
 function App() {
+
+  let[todos,setTodos] = useState<Array<PropsType>>([])
+
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/posts/')
+            .then((response) => response.json())
+            .then((json) => setTodos(json))
+    },[])
+
+    const onclickHandler = () => {
+      setTodos([])
+    }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={onclickHandler}>clean posts</button>
+      <ul>
+        {todos.map(el=>{
+          return(
+              <li>
+                  <span>{el.userId}</span>
+                  <span> {el.title}</span>
+                  <span> {el.id}</span>
+                  <span> {el.completed}</span>
+              </li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
